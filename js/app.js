02c7e55cc1e339f4faa7a64cfb9152df2875dd7e
Dima <dmitriyy.pryzov@gmail.com>
2024-02-07 13:01:43 +0200

    adaptive version

---
+++ js/app.js
@@ -0,0 +1,11 @@
+"use strict"
+
+const burgerMenu = document.querySelector(".burger-menu");
+const headerMenu = document.querySelector(".header-menu");
+
+burgerMenu.addEventListener("click", () => {
+    burgerMenu.classList.toggle("active");
+    headerMenu.classList.toggle("active");
+    document.body.classList.toggle("active");
+});
+
